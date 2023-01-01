b403400436e0840e2316b440eb3fec3bf7edcbe4
MarcoHaber <marcoshaber99@gmail.com>
2023-01-01 23:52:05 +0200

    ffs

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function App() {
+  return (
+    <main className="grid place-items-center min-h-screen bg-gradient-to-b from-slate-100 to-slate-200 dark:from-slate-800 dark:to-slate-900 text-slate-900 dark:text-slate-200">
+      <h1>what is your one thing?</h1>
+    </main>
+  )
+}
+
+export default App
